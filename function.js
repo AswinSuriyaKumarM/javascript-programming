2d20b9c41024ea7df22b528bd51611f1b7507f75
AswinSuriyaKumarM <aswinsuriyaKumar@gmail.com>
2024-06-06 15:31:33 +0530

    third commit

---
+++ function.js
@@ -0,0 +1,34 @@
+var a = 10
+var b = 20
+
+function add() {
+    console.log(a + b)
+}
+
+add()
+
+var fActor = "kamal"
+var fPlayer = "dhoni"
+var fMovie = "anbe sivam"
+
+function favourite() {
+    console.log("fActor: " + fActor)
+    console.log("fPlayer: " + fPlayer)
+    console.log("fMovie: " + fMovie)
+}
+
+favourite()
+
+function area(length, breadth) {
+    var area = length * breadth
+    console.log(area)
+}
+
+area(10, 20)
+
+function add(a, b){
+    return a+b;
+}
+
+var result = add(3,5)
+console.log(result)
--- ifelse.js
+++ ifelse.js
@@ -31,3 +31,41 @@ if (true) {
 } else {
     console.log("Have a great day")
 }
+
+var season = "fall"
+
+if(season == "spring"){
+    console.log("Enjoy the blooming flowers")
+} else if(season == "summer"){
+    console.log("Have fun in sun")
+} else if (season == "fall" || season == "autumn"){
+    console.log("Admire the colorful leaves")
+} else {
+    console.log("bundle up and stay warm")
+}
+
+var score = 80
+
+if(score < 50){
+    console.log("you need to improve")
+} else if(score >= 50 && score <= 70){
+    console.log("good job")
+} else {
+    console.log("excellent performance")
+}
+
+var value = 15
+
+if (value % 2 == 0) {
+    console.log("even number")
+} else {
+    console.log("odd number")
+}
+
+var character = "b"
+
+if (character == "a" ||character == "e" ||character == "i" ||character == "o" ||character == "u") {
+    console.log("Vowel")
+} else {
+    console.log("Not vowel")
+}
