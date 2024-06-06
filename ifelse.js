6227418f151abf0d16d75b923c5ca4bdd78f0127
AswinSuriyaKumarM <aswinsuriyaKumar@gmail.com>
2024-06-06 12:46:09 +0530

    second commit

--- ifelse.js
+++ ifelse.js
@@ -1,3 +1,33 @@
-if () {
-console.log("I")
+if (true) {
+    console.log("It's raining")
+}
+
+if (true) {
+    console.log("Take an umbrella")
+} else {
+    console.log("Enjoy the sunshine")
+}
+
+if (false) {
+    console.log("Great job!")
+} else {
+    console.log("Finish your homework before playing")
+}
+
+if (true) {
+    console.log("Would you like a cookie?")
+} else {
+    console.log("Time to bake more cookies.")
+}
+
+if (true) {
+    console.log("Your teeth are clean")
+} else {
+    console.log("Go brush your teeth now")
+}
+
+if (true) {
+    console.log("Happy birthday")
+} else {
+    console.log("Have a great day")
 }
